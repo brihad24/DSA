25c3a5123e6655e7dfcccc6672c37770714fb825
brihad24 <brihad1692@gmail.com>
2023-07-05 01:10:34 +0530

    LeetCode BitWise Operators

---
+++ LeetCode/Bitwise Operators/FlipImage.java
@@ -0,0 +1,31 @@
+// Question: https://leetcode.com/problems/flipping-an-image/
+
+import java.util.Arrays;
+
+public class FlipImage {
+    public static void main(String[] args) {
+        int[][] image = {{1,1,0}
+                        ,{1,0,1}
+                        ,{0,0,0}};
+        int[][] newImage = flipAndInvertImage(image);
+        for(int[] row : newImage)
+        {
+            System.out.println(Arrays.toString(row));
+        }
+        // System.out.println(flipAndInvertImage(image));
+    }
+
+    static int[][] flipAndInvertImage(int[][] image) {
+        for(int[] row : image)
+        {
+            for(int i=0; i < (image[0].length + 1) / 2; i++)
+            {
+                int temp = row[i] ^ 1;
+                row[i] = row[image[0].length - i - 1] ^ 1;
+                row[image[0].length - i - 1] = temp;
+            }
+        }
+
+        return image;
+    }
+}
